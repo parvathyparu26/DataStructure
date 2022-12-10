1fe004b9742748685c93fa7c71d8e3125fdc7628
parvathyparu26 <117814552+parvathyparu26@users.noreply.github.com>
2022-12-10 04:26:02 -1200

    Create Average.c
    Experiment number 2

---
+++ Average.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c,avg;
+printf("enter the numbers");
+scanf("%d %d %d",&a,&b,&c);
+avg=(a+b+c)/3;
+printf("avg=%d",avg);
+}
+output
+Enter the numbers 2
+  3
+  4 
+  avg=3

31159b033c18eb329c94d89ac92576c3a91b33f2
parvathyparu26 <117814552+parvathyparu26@users.noreply.github.com>
2022-12-11 02:45:57 -1200

    Create array.c
    Experiment number 6

---
+++ array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>  
+  
+void  main()  
+{  
+    int a[10],i,n;
+    printf("enter the limit");
+    scanf("%d",&n);
+    printf("enter the element");
+    for(i=0;i<n;i++)
+    {
+        printf("%d",i);
+        scanf("%d",&a[i]);
+    }
+    printf("the array is");
+    for(i=0;i<n;i++)
+    printf("%d",a[i]);
+}
+output
+enter the limit5
+enter the element01 2 3 4 5
+the array is12345
